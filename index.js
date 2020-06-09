f895021308983e333ee295e2b79532a2b23b7c02
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2020-06-09 14:01:44 +0300

    feat: initial

---
+++ index.js
@@ -0,0 +1,37 @@
+const hapi = require('@hapi/hapi');
+
+module.exports = (...params) => class voicecomsim extends require('ut-port-script')(...params) {
+    get defaults() {
+        return {
+            server: {
+                port: 8086
+            }
+        };
+    }
+
+    handlers() {
+        let server;
+        return {
+            start: async() => {
+                server = new hapi.Server(this.config.server);
+                server.route({
+                    path: '/multichannel-api/sendmulti/',
+                    method: 'post',
+                    handler: async(req, h) => {
+                        return h.response({
+                            return_code: 0
+                        });
+                    }
+                });
+                await server.start();
+            },
+            stop: async() => {
+                if (server) {
+                    const stopping = server;
+                    server = false;
+                    await stopping.stop();
+                }
+            }
+        };
+    }
+};
